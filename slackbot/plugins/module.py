e54bd8c96f02321726f7b7c816b3dfab1fbd30cc
kusano <kusakoubump@gmail.com>
2020-04-06 01:09:20 +0900

    ひとまず完成

--- slackbot/plugins/module.py
+++ slackbot/plugins/module.py
@@ -1,27 +1,39 @@
 from slackbot.bot import respond_to
 from slackbot.bot import listen_to
 from slackbot.bot import default_reply
+from slacker import Slacker
+import slackbot_settings
 import requests
+import os
+
+slacker = Slacker(slackbot_settings.API_TOKEN)
+channel = 'fanart'
+file = '.logs.txt'
 
 
-# # 「respond_to」はメンションする(@でターゲットを指定すること)と応答する
-# @respond_to('こんにちは')
-# def greeting_1(message):
-#     # Slackに応答を返す
-#     message.reply('こんにちは!')
+# @respond_to('del')
+# def del_logs(message):
+#     slacker.files.upload(file_=file, channel=channel)
+#     os.system('echo '' > .logs.txt')
+#     message.reply('delete logs')
 
 
-# # 「listen_to」はメンションがなくても応答する
-# @listen_to('コニチハ')
-# def greeting_2(message):
-#     message.reply('コニチハ')
+@respond_to('sum')
+def up_summary(message):
+    slacker.files.upload(file_=file, channels=channel)
+    message.send('ファイルをアップロードしました')
+    os.system('echo '' > .logs.txt')
+    message.reply('delete logs')
+
 
 @default_reply
 def default_reply(message):
     url = message.body['text'].strip('<').strip('>')
     html = get_html(url)
-
     message.reply(html)
+
+    with open('.logs.txt', mode='a') as f:
+        f.write(html)
 
 
 def get_html(tweet_url):
