32842e92600b156410213dab5d8a99d97ff5c204
kusano <kusakoubump@gmail.com>
2020-04-12 00:09:36 +0900

    fixed del sum command

--- slackbot/plugins/module.py
+++ slackbot/plugins/module.py
@@ -7,14 +7,43 @@ import requests
 import os
 
 slacker = Slacker(slackbot_settings.API_TOKEN)
-channel = 'fanart'
-file_name = '.logs.txt'
+# channel = 'fanart'
+# file_name = '.logs.txt'
 
 
 @listen_to(r'twitter')
 def get_tw_link(message):
     ch = get_channel(message)
     logs_overwrite(message, ch)
+
+
+@listen_to('del')
+def del_logs(message):
+    ch = get_channel(message)
+    os.system('echo '' > .logs_{}.txt'.format(ch))
+    message.send('delete logs')
+
+
+@listen_to('sum')
+def up_summary(message):
+    ch = get_channel(message)
+    file_name = '.logs_{}.txt'.format(ch)
+    slacker.files.upload(file_=file_name, channels=ch)
+    message.send('ファイルをアップロードしました')
+    os.system('echo '' > .logs_{}.txt'.format(ch))
+    message.send('delete logs')
+
+
+def get_html(tweet_url):
+    params = (
+        ('url', tweet_url),
+        ('data-conversation', 'none'),
+        ('lang', 'ja'),
+    )
+    response = requests.get(
+        'https://publish.twitter.com/oembed', params=params)
+    html = "".join(response.json()['html'].split('\n'))
+    return html
 
 
 def get_channel(message):
@@ -32,45 +61,3 @@ def logs_overwrite(message, ch):
 
     with open('.logs_{}.txt'.format(ch), mode='a')as f:
         f.write(html)
-
-
-# @respond_to('del')
-@listen_to('del')
-def del_logs(message):
-    ch = get_channel(message)
-    os.system('echo '' > .logs{}.txt'.format(ch))
-    message.send('delete logs')
-
-
-# @respond_to('sum')
-@listen_to('sum')
-def up_summary(message):
-    ch = get_channel(message)
-    slacker.files.upload(file_=file_name, channels=ch)
-    message.send('ファイルをアップロードしました')
-    os.system('echo '' > .logs{}.txt'.format(ch))
-    message.send('delete logs')
-
-
-# @default_reply()
-# def default(message):
-#     import pdb
-#     pdb.set_trace()
-#     url = message.body['text'].strip('<').strip('>')
-#     html = get_html(url)
-#     message.send(html)
-
-#     with open('.logs.txt', mode='a') as f:
-#         f.write(html)
-
-
-def get_html(tweet_url):
-    params = (
-        ('url', tweet_url),
-        ('data-conversation', 'none'),
-        ('lang', 'ja'),
-    )
-    response = requests.get(
-        'https://publish.twitter.com/oembed', params=params)
-    html = "".join(response.json()['html'].split('\n'))
-    return html
