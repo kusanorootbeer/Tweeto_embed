351fb1c436a1a3e0977ffc61aaba04322aa20584
kusano <kusakoubump@gmail.com>
2020-04-12 00:00:15 +0900

    separete log file by channel name

--- slackbot/plugins/module.py
+++ slackbot/plugins/module.py
@@ -8,32 +8,60 @@ import os
 
 slacker = Slacker(slackbot_settings.API_TOKEN)
 channel = 'fanart'
-file = '.logs.txt'
+file_name = '.logs.txt'
 
 
-@respond_to('del')
-def del_logs(message):
-    slacker.files.upload(file_=file, channel=channel)
-    os.system('echo '' > .logs.txt') 
-    message.reply('delete logs')
+@listen_to(r'twitter')
+def get_tw_link(message):
+    ch = get_channel(message)
+    logs_overwrite(message, ch)
 
 
-@respond_to('sum')
-def up_summary(message):
-    slacker.files.upload(file_=file, channels=channel)
-    message.send('ファイルをアップロードしました')
-    os.system('echo '' > .logs.txt')
-    message.reply('delete logs')
+def get_channel(message):
+    try:
+        channel_name = message.channel._body['name']
+    except:
+        channel_name = ''
+    return channel_name
 
 
-@default_reply
-def default_reply(message):
+def logs_overwrite(message, ch):
     url = message.body['text'].strip('<').strip('>')
     html = get_html(url)
-    message.reply(html)
+    message.send(html)
 
-    with open('.logs.txt', mode='a') as f:
+    with open('.logs_{}.txt'.format(ch), mode='a')as f:
         f.write(html)
+
+
+# @respond_to('del')
+@listen_to('del')
+def del_logs(message):
+    ch = get_channel(message)
+    os.system('echo '' > .logs{}.txt'.format(ch))
+    message.send('delete logs')
+
+
+# @respond_to('sum')
+@listen_to('sum')
+def up_summary(message):
+    ch = get_channel(message)
+    slacker.files.upload(file_=file_name, channels=ch)
+    message.send('ファイルをアップロードしました')
+    os.system('echo '' > .logs{}.txt'.format(ch))
+    message.send('delete logs')
+
+
+# @default_reply()
+# def default(message):
+#     import pdb
+#     pdb.set_trace()
+#     url = message.body['text'].strip('<').strip('>')
+#     html = get_html(url)
+#     message.send(html)
+
+#     with open('.logs.txt', mode='a') as f:
+#         f.write(html)
 
 
 def get_html(tweet_url):
